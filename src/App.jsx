0f49fd94d2d1db0c4e9181cd310a0c7ba1c5dce0
Jochyd <mail>
2022-09-28 20:57:05 -0300

    realice el maquetado del form

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { Container } from "react-bootstrap";
 import FormCard from "./Components/FormCard";
 
 function App() {
   return (
    <>
-   <Container>
-    <h1 className="display-5 text-center my-5">
-      Colores
-    </h1>
-    <hr />
     <FormCard></FormCard>
-   </Container>
    </>
   );
 }
